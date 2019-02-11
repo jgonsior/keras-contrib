c4ae49620f730d8d8b2e7ae898a283e0e6f7413f
Julius Gonsior <jgonsior@avantgarde-labs.de>
2019-02-11 18:42:54 +0100

    Removed output thingies

--- keras_contrib/layers/recurrent.py
+++ keras_contrib/layers/recurrent.py
@@ -234,13 +234,15 @@ class InsideLSTMCell(Layer):
             c = f * c_tm1 + i * self.activation(z2)
             o = self.recurrent_activation(z3)
 
-            if self.activationGate is not None:
-                i = print_tensor_long(i, message=self.name + "_i" + ": ")
-                f = print_tensor_long(f, message=self.name + "_f" + ": ")
-                c = print_tensor_long(c, message=self.name + "_c" + ": ")
-                #  o = print_tensor_long(o, message=self.name + "_o" + ": ")
-
         h = o * self.activation(c)
+
+        if self.activationGate is not None:
+            i = print_tensor_long(i, message=self.name + "_i" + ": ")
+            f = print_tensor_long(f, message=self.name + "_f" + ": ")
+            #  c = print_tensor_long(c, message=self.name + "_c" + ": ")
+            #  o = print_tensor_long(o, message=self.name + "_o" + ": ")
+            h = print_tensor_long(h, message=self.name + "_h" + ": ")
+
         if 0 < self.dropout + self.recurrent_dropout:
             if training is None:
                 h._uses_learning_phase = True
